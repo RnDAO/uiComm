f5d5ab4344fbb11416a3b7699ba83607ef248357
zuies <zurabichkhikvadze66@gmail.com>
2022-11-10 12:56:03 +0400

    add hightcharts

---
+++ src/components/pages/pageIndex/MainSection.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Chart from "../../global/Chart";
+type Props = {};
+
+const MainSection = (props: Props) => {
+  return (
+    <div>
+      MainSection
+      <Chart />
+    </div>
+  );
+};
+
+export default MainSection;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { RowOne } from "../components/pages/pageIndex/RowOne";
 import { FooterSection } from "../components/pages/pageIndex/FooterSection";
+import MainSection from "../components/pages/pageIndex/MainSection";
 export default function PageIndex(): JSX.Element {
   return (
     <>
       <div className="flex flex-col space-y-12 justify-between px-12 py-4">
         <RowOne />
+        <MainSection />
         <FooterSection />
       </div>
     </>
