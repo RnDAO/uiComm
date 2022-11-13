b8375e5160f5a8c645aeb5def115a1624cfc8a4e
zuies <zurabichkhikvadze66@gmail.com>
2022-11-13 12:01:12 +0400

    change some styles

--- src/layouts/defaultLayout.tsx
+++ src/layouts/defaultLayout.tsx
@@ -9,10 +9,10 @@ export const defaultLayout = ({ children }: Props) => {
   return (
     <>
       <div className="w-screen flex flex-row justify-between w-full">
-        <aside className="bg-gray-background shadow-inner w-28 shadow-inner fixed min-h-screen">
+        <aside className="bg-gray-background shadow-inner w-24 shadow-inner fixed min-h-screen">
           <Sidebar />
         </aside>
-        <main className="ml-28 flex-1">{children}</main>
+        <main className="ml-24 flex-1">{children}</main>
       </div>
     </>
   );
