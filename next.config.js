f8d076e15e661f8a3612d12d02473f71c041ec87
zuies <zurabichkhikvadze66@gmail.com>
2022-11-22 13:07:46 +0400

    add image unoptimized option to next.config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  exportTrailingSlash: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
