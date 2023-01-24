514537b0916b127332a4decbe1851e731b4b0650
zuies <zurabichkhikvadze66@gmail.com>
2023-01-24 18:18:49 +0400

    change http to https

--- src/components/global/LoadingBackdrop.tsx
+++ src/components/global/LoadingBackdrop.tsx
@@ -5,7 +5,7 @@ export default function SimpleBackdrop() {
   return (
     <Backdrop
       open={true}
-      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      sx={{ color: '#804EE1', zIndex: (theme) => theme.zIndex.drawer + 1 }}
     >
       <CircularProgress color="inherit" />
     </Backdrop>
