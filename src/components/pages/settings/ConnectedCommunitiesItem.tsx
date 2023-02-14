59f8e1bb251035fa75026f2d5a18b2d80eefd55e
zuies <zurabichkhikvadze66@gmail.com>
2023-02-14 16:44:22 +0400

    fix some style in connectedCommiunitiesItem component

--- src/components/pages/settings/ConnectedCommunitiesItem.tsx
+++ src/components/pages/settings/ConnectedCommunitiesItem.tsx
@@ -60,10 +60,10 @@ export default function ConnectedCommunitiesItem({ guild, onClick }: IProps) {
               width="100"
               height="100"
               alt={guild.name ? guild.name : ''}
-              className="rounded-full"
+              className="rounded-full w-9 h-9"
             />
           ) : (
-            <div className="bg-secondary text-center  w-9 h-9 rounded-full align-center flex flex-col justify-center text-xs" />
+            <div className="bg-secondary text-center w-9 h-9 rounded-full align-center flex flex-col justify-center text-xs" />
           )}
           <div className="flex flex-col text-left pl-1">
             <p className="font-bold">{guild.name}</p>
