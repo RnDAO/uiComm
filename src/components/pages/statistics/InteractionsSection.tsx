fd4f8f12d879f0b2556d9f77f3e76f17962837d3
zuies <zurabichkhikvadze66@gmail.com>
2023-04-07 15:45:15 +0400

    fix conflicts

--- src/components/pages/statistics/InteractionsSection.tsx
+++ src/components/pages/statistics/InteractionsSection.tsx
@@ -37,29 +37,6 @@ const defaultOptions = {
   },
 };
 
-const communityActiveDates = [
-  {
-    title: 'Last 7 days',
-    value: 1,
-  },
-  {
-    title: '1M',
-    value: 2,
-  },
-  {
-    title: '3M',
-    value: 3,
-  },
-  {
-    title: '6M',
-    value: 4,
-  },
-  {
-    title: '1Y',
-    value: 5,
-  },
-];
-
 export default function InteractionsSection() {
   const { interactions } = useAppStore();
   const [options, setOptions] = useState(defaultOptions);
