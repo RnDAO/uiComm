9116c5ff244c98e4403816a723392574d7a9805b
zuies <zurabichkhikvadze66@gmail.com>
2023-04-17 11:49:39 +0400

    add Mava

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,12 +23,29 @@ import { conf } from '../configs';
 
 export default function App({ Component, pageProps }: ComponentWithPageLayout) {
   useEffect(() => {
+    // Get Hotjar ID from configuration
     const HOTJAR_ID: string | undefined = conf.HOTJAR_ID;
 
+    // Initialize Hotjar tracking in production environment
     if (process.env.NODE_ENV === 'production' && HOTJAR_ID) {
       const hotjarIdAsNumber = parseInt(HOTJAR_ID, 10);
       hotjar.initialize(hotjarIdAsNumber, 6);
     }
+
+    // Add MavaWebChat script to the document body
+    const script = document.createElement('script');
+    script.src = 'https://widget.mava.app';
+    script.id = 'MavaWebChat';
+    script.setAttribute(
+      'data-token',
+      '4b5905555f806bba755efb553116f0a9b56d0c68c8c0fca78ce171332c61c372'
+    );
+    document.body.appendChild(script);
+
+    // Remove MavaWebChat script from the document body when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
   }, []);
 
   return (
