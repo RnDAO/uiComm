e43a744a50a07913fcbb4c90bdb1c647b537c440
zuies <zurabichkhikvadze66@gmail.com>
2023-09-08 15:36:01 +0300

    change some styles

--- src/components/pages/statistics/memberBreakdowns/activeMembers/ActiveMemberBreakdown.tsx
+++ src/components/pages/statistics/memberBreakdowns/activeMembers/ActiveMemberBreakdown.tsx
@@ -149,7 +149,7 @@ export default function ActiveMemberBreakdown() {
   return (
     <>
       <div className="relative overflow-x-scroll md:overflow-hidden mb-1 md:mb-1">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-start">
           <h3
             className="text-xl font-medium text-lite-black md:mb-4"
             ref={tableTopRef}
