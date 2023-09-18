2fd68d012a721ec40c1f985d0eb6c8a21e7e15a1
zuies <zurabichkhikvadze66@gmail.com>
2023-09-18 18:41:48 +0300

    change the color

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from '@mui/material/styles';
 export const theme = createTheme({
   palette: {
     primary: {
-      main: '#8054DE',
+      main: '#804EE1',
     },
   },
   typography: {
