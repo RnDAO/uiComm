10a5a02310618175ef76dddddd9dc7714fa0055d
zuies <zurabichkhikvadze66@gmail.com>
2023-09-26 15:59:51 +0300

    add apies

--- src/store/slices/twitterSlice.ts
+++ src/store/slices/twitterSlice.ts
@@ -1,28 +1,45 @@
 import { StateCreator } from 'zustand';
 import { axiosInstance } from '../../axiosInstance';
 import ITwitter from '../types/ITwitter';
-import { conf } from '../../configs';
-
-const BASE_URL = conf.API_BASE_URL;
 
 const createTwitterSlice: StateCreator<ITwitter> = (set, get) => ({
   authorizeTwitter: async (token: string) => {
     try {
-      // Send token to intermediary endpoint
-      await axiosInstance({
-        method: 'GET', // adjust as necessary
-        url: `${BASE_URL}/auth/twitter/login`,
+      await axiosInstance.get(`/auth/twitter/login`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
-
-      // The above request should set some session or cookie authentication
-      // and then redirect to the Twitter authorization URL.
-      // Since it's a 302 redirect, you might not need to handle the response directly.
     } catch (error) {
       console.error('Error in intermediary auth step:', error);
     }
+  },
+  disconnectTwitter: async () => {
+    try {
+      await axiosInstance.post(`/twitter/disconnct`);
+    } catch (error) {}
+  },
+  refreshTwitterMetrics: async (username) => {
+    try {
+      await axiosInstance.post(`/twitter/metrics/refresh`, {
+        twitter_username: username,
+      });
+    } catch (error) {}
+  },
+  twitterActivityAccount: (twitterId) => {
+    try {
+      axiosInstance.get(`/twitter/${twitterId}/metrics/activity`);
+    } catch (error) {}
+  },
+  twitterAudienceAccount: (twitterId) => {
+    try {
+      axiosInstance.get(`/twitter/${twitterId}/metrics/audience`);
+    } catch (error) {}
+  },
+  twitterEngagementAccount: (twitterId) => {
+    try {
+      axiosInstance.get(`/twitter/${twitterId}/metrics/engagement`);
+    } catch (error) {}
   },
 });
 
--- src/store/types/ITwitter.ts
+++ src/store/types/ITwitter.ts
@@ -1,3 +1,8 @@
 export default interface ITwitter {
   authorizeTwitter: (token: string) => void;
+  disconnectTwitter: () => void;
+  refreshTwitterMetrics: (username: string) => void;
+  twitterActivityAccount: (twitterId: string) => void;
+  twitterAudienceAccount: (twitterId: string) => void;
+  twitterEngagementAccount: (twitterId: string) => void;
 }
