d067d4d8021e82d20a964908b71ca7abfd267ef1
zuies <zurabichkhikvadze66@gmail.com>
2023-11-02 17:40:15 +0400

    change some texts

--- src/components/communitySettings/communityIntegrations/TcCommunityIntegrations.tsx
+++ src/components/communitySettings/communityIntegrations/TcCommunityIntegrations.tsx
@@ -8,12 +8,18 @@ function TcCommunityIntegrations() {
     <>
       <TcText text="Integration" variant={'h6'} />
       <div className="space-y-5">
-        <div className="space-y-4">
-          <TcText text="Connected platforms" variant="body2" />
+        <div className="space-y-4 md:mb-8">
+          <TcText
+            text="Platforms currently connected to your community account"
+            variant="body2"
+          />
           <TcConnectedPlatforms />
         </div>
         <div className="space-y-4">
-          <TcText text="Available integrations" variant="body2" />
+          <TcText
+            text="Chose the platforms you want to connect with your community account"
+            variant="body2"
+          />
           <TcAvailableIntegrations />
         </div>
       </div>
