0366eeafba5ab4f9ddf4453471ef26b5daf8a3ba
zuies <zurabichkhikvadze66@gmail.com>
2023-11-13 12:02:12 +0300

    implement tc-select component

---
+++ src/components/shared/TcSelect/TsSelect.spec.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import TcSelect from './TcSelect';
+
+describe('TcSelect Component', () => {
+  const mockOptions = [
+    { value: '1', label: 'Option 1' },
+    { value: '2', label: 'Option 2' },
+  ];
+
+  test('renders the select component', () => {
+    render(<TcSelect options={mockOptions} />);
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  test('can select an option', () => {
+    render(<TcSelect options={mockOptions} />);
+    fireEvent.mouseDown(screen.getByRole('button'));
+    const option = screen.getByText('Option 1');
+    fireEvent.click(option);
+    expect(screen.getByRole('button').textContent).toBe('Option 1');
+  });
+});
---
+++ src/components/shared/TcSelect/index.ts
@@ -0,0 +1,3 @@
+import { default as TcSelect } from './TcSelect';
+
+export default TcSelect;
