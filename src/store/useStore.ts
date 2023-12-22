95c80f917daeb5fb7488bf214ee341625cd68de6
zuies <zurabichkhikvadze66@gmail.com>
2023-12-22 18:05:40 +0300

    remove settingSlice & authSlice

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,7 +1,5 @@
 import { create } from 'zustand';
-import createAuthSlice from './slices/authSlice';
 import createChartSlice from './slices/chartSlice';
-import createSettingSlice from './slices/settingSlice';
 import createBreakdownsSlice from './slices/breakdownsSlice';
 import createMemberInteractionSlice from './slices/memberInteractionSlice';
 import communityHealthSlice from './slices/communityHealthSlice';
@@ -11,9 +9,7 @@ import platformSlice from './slices/platformSlice';
 import userSlice from './slices/userSlice';
 
 const useAppStore = create<any>()((...a) => ({
-  ...createAuthSlice(...a),
   ...createChartSlice(...a),
-  ...createSettingSlice(...a),
   ...createBreakdownsSlice(...a),
   ...createMemberInteractionSlice(...a),
   ...communityHealthSlice(...a),
