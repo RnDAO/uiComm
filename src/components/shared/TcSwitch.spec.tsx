2c21184657db225098b98ed73b28bb291b7f906e
zuies <zurabichkhikvadze66@gmail.com>
2023-12-27 13:30:45 +0300

    add TcSwitch component

---
+++ src/components/shared/TcSwitch.spec.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import TcSwitch from './TcSwitch';
+
+describe('TcSwitch', () => {
+  test('it should toggle switch', () => {
+    const handleChange = jest.fn();
+    const { getByRole } = render(<TcSwitch onChange={handleChange} />);
+
+    const switchControl = getByRole('checkbox');
+    expect(switchControl).not.toBeChecked();
+
+    fireEvent.click(switchControl);
+    expect(handleChange).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/components/shared/TcSwitch.tsx
@@ -0,0 +1,37 @@
+import { Switch, SwitchProps } from '@mui/material';
+import React from 'react';
+
+interface ITcSwitchProps extends SwitchProps {}
+
+/**
+ * `TcSwitch` Component
+ *
+ * This component is a wrapper around Material-UI's `Switch` component.
+ * It can be used anywhere a Material-UI Switch would be used. It accepts all props
+ * that a standard Material-UI Switch accepts.
+ *
+ * Usage:
+ * <TcSwitch checked={value} onChange={handleChange} />
+ *
+ * Props:
+ * - All props available to Material-UI's `Switch` component.
+ * - `checked`: Boolean indicating whether the switch is on or off.
+ * - `onChange`: Function to handle the change event when the switch is toggled.
+ *
+ * Example:
+ * ```
+ * <TcSwitch
+ *   checked={this.state.isChecked}
+ *   onChange={(e) => { this.setState({ isChecked: e.target.checked }) }}
+ * />
+ * ```
+ *
+ * For more details on Material-UI's `Switch` props,
+ * see: https://mui.com/api/switch/
+ */
+
+function TcSwitch({ ...props }: ITcSwitchProps) {
+  return <Switch {...props} />;
+}
+
+export default TcSwitch;
