beeaa133105dac89fe25514d997930671b06399a
mehditorabiv <mehditorabiv@gmail.com>
2024-11-27 15:20:47 +0300

    update config

--- next.config.js
+++ next.config.js
@@ -5,9 +5,8 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: true,
   images: {
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [320, 420, 768, 1024, 1200],
-    disableStaticImages: false,
+    loader:'custom',
+    unoptimized: true,
   },
 };
 
