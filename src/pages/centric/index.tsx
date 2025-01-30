63263f0821f2626e5f2e25002f4a3acfe7be678b
Mehdi Torabi <46302001+mehdi-torabiv@users.noreply.github.com>
2025-01-30 18:10:04 +0300

    add telegram (#387)
    * add telegram
    
    * add log
    
    * update
    
    * add requestaccess write

--- src/pages/centric/index.tsx
+++ src/pages/centric/index.tsx
@@ -1,44 +1,58 @@
-import React from 'react';
+import React from "react";
+import { LoginButton } from "@telegram-auth/react";
 
-import TcBoxContainer from '../../components/shared/TcBox/TcBoxContainer';
-import TcButton from '../../components/shared/TcButton';
-import TcText from '../../components/shared/TcText';
-import centricLayout from '../../layouts/centricLayout';
-import useAppStore from '../../store/useStore';
+import { conf } from "@/configs";
+
+import TcBoxContainer from "../../components/shared/TcBox/TcBoxContainer";
+import TcButton from "../../components/shared/TcButton";
+import TcText from "../../components/shared/TcText";
+import centricLayout from "../../layouts/centricLayout";
+import useAppStore from "../../store/useStore";
 
 function Index() {
-  const { discordAuthorization } = useAppStore();
-  return (
-    <div>
-      <TcBoxContainer
-        bgcolor='white'
-        className='rounded py-12 px-4 md:min-h-[37.5rem] md:p-12'
-        contentContainerChildren={
-          <div className='space-y-8 py-12'>
-            <TcText
-              sx={{ typography: { xs: 'h5', md: 'h4' } }}
-              color='initial'
-              fontWeight='bold'
-              text='Log in'
-            />
-            <div className='block'>
-              <TcButton
-                text='Log in with Discord'
-                sx={{ width: '15rem', padding: '0.5rem' }}
-                variant='contained'
-                onClick={() => discordAuthorization()}
-              />
-            </div>
-            <TcText
-              variant='body1'
-              color='initial'
-              text='More log-in options Coming soon.'
-            />
-          </div>
-        }
-      />
-    </div>
-  );
+	console.log(conf.TELEGRAM_BOT_USERNAME);
+
+	const { discordAuthorization } = useAppStore();
+	return (
+		<div>
+			<TcBoxContainer
+				bgcolor="white"
+				className="rounded py-12 px-4 md:min-h-[37.5rem] md:p-12"
+				contentContainerChildren={
+					<div className="space-y-8 py-12">
+						<TcText
+							sx={{ typography: { xs: "h5", md: "h4" } }}
+							color="initial"
+							fontWeight="bold"
+							text="Log in"
+						/>
+						<div className="block">
+							<TcButton
+								text="Log in with Discord"
+								sx={{ width: "15rem", padding: "0.5rem" }}
+								variant="contained"
+								onClick={() => discordAuthorization()}
+							/>
+							<LoginButton
+								botUsername="Togethercrew_Staging_Bot"
+								authCallbackUrl="https://api.togethercrew.de/api/v1/auth/telegram/authorize/callback"
+								buttonSize="large" // "large" | "medium" | "small"
+                requestAccess="write"
+								cornerRadius={5} // 0 - 20
+								showAvatar={true} // true | false
+								lang="en"
+							/>
+						</div>
+						<TcText
+							variant="body1"
+							color="initial"
+							text="More log-in options Coming soon."
+						/>
+					</div>
+				}
+			/>
+		</div>
+	);
 }
 
 Index.pageLayout = centricLayout;
